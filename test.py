ba8f43ec83ad25168ef5e55cd1ce8805b7e2e7e4
Alexandru Rosianu <me@aluxian.com>
2017-03-19 03:06:19 +0100

    face recognition done

--- test.py
+++ test.py
@@ -3,18 +3,10 @@ import sys
 
 from time import sleep
 
-cascPath = "haarcascade_frontalface_default.xml"
-faceCascade = cv2.CascadeClassifier(cascPath)
-
+faceCascade = cv2.CascadeClassifier("shape_predictor_68_face_landmarks.dat")
 video_capture = cv2.VideoCapture(0)
-anterior = 0
 
 while True:
-    if not video_capture.isOpened():
-        print('Unable to load camera.')
-        sleep(5)
-        pass
-
     # Capture frame-by-frame
     ret, frame = video_capture.read()
 
@@ -30,9 +22,6 @@ while True:
     # Draw a rectangle around the faces
     for (x, y, w, h) in faces:
         cv2.rectangle(frame, (x, y), (x+w, y+h), (0, 255, 0), 2)
-
-    if anterior != len(faces):
-        anterior = len(faces)
 
     if cv2.waitKey(1) & 0xFF == ord('q'):
         break
