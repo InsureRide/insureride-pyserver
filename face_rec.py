000ea77d2994adbe2f27e8f0a2cdf7409056c9a6
Alexandru Rosianu <me@aluxian.com>
2017-03-19 06:30:50 +0100

    facial recognition works, woooohooo!

---
+++ face_rec.py
@@ -0,0 +1,49 @@
+import face_recognition
+import cv2
+
+# Get a reference to webcam #0 (the default one)
+video_capture = cv2.VideoCapture(0)
+
+# Load a sample picture and learn how to recognize it.
+known_faces = [
+    face_recognition.load_image_file("known_people/alex.jpg")
+]
+known_faces_encodings = [face_recognition.face_encodings(img)[0] for img in known_faces]
+known_faces_names = ["Alex"]
+
+while True:
+    # Grab a single frame of video
+    ret, frame = video_capture.read()
+
+    # Find all the faces and face enqcodings in the frame of video
+    face_locations = face_recognition.face_locations(frame)
+    face_encodings = face_recognition.face_encodings(frame, face_locations)
+
+    # Loop through each face in this frame of video
+    for (top, right, bottom, left), face_encoding in zip(face_locations, face_encodings):
+        # See if the face is a match for the known face(s)
+        match = face_recognition.compare_faces(known_faces_encodings, face_encoding)
+
+        name = "Unknown"
+        for i, m in enumerate(match):
+            if m:
+                name = known_faces_names[i]
+
+        # Draw a box around the face
+        cv2.rectangle(frame, (left, top), (right, bottom), (0, 0, 255), 2)
+
+        # Draw a label with a name below the face
+        cv2.rectangle(frame, (left, bottom - 35), (right, bottom), (0, 0, 255), 2)
+        font = cv2.FONT_HERSHEY_DUPLEX
+        cv2.putText(frame, name, (left + 6, bottom - 6), font, 1.0, (255, 255, 255), 1)
+
+    # Display the resulting image
+    cv2.imshow('Insure Ride', frame)
+
+    # Hit 'q' on the keyboard to quit!
+    if cv2.waitKey(1) & 0xFF == ord('q'):
+        break
+
+# Release handle to the webcam
+video_capture.release()
+cv2.destroyAllWindows()
